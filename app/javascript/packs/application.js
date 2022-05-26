f0539b93154b126276ad68fa595bcfefafc32dab
Thado Zalawmpuia <zlwmpthado@gmail.com>
2022-05-27 00:20:43 +0900

    add bootstrap for styling and add navi and flash messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import 'bootstrap';
-import '../stylesheets/application';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +16,5 @@ import '../stylesheets/application';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap";
